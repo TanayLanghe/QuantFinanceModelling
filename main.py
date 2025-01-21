cf05a2d703e0e85ad22a14a4b19edcfc7be0139d
Tanay Langhe <tanaylanghe@gmail.com>
2025-01-21 15:39:08 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,80 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+
+class Market:
+    transaction_fee = 0.005
+    def __init__(self) -> None:
+        self.stocks = {"HydroCorp": 123, "BrightFuture": 456}
+
+    def updateMarket(self):
+        #Will be implemented during grading.
+        #This function will update the stock values to their "real" values each day.
+        pass
+
+class Portfolio:
+    def __init__(self) -> None:
+        self.shares = {"HydroCorp": 0, "BrightFuture": 0}
+        self.cash = 100000
+
+    def evaluate(self, curMarket: Market) -> float:
+        valueA = self.shares["HydroCorp"] * curMarket.stocks["HydroCorp"]
+        valueB = self.shares["BrightFuture"] * curMarket.stocks["BrightFuture"]
+
+        return valueA + valueB + self.cash
+
+    def sell(self, stock_TSLA: str, sharesToSell: float, curMarket: Market) -> None:
+        if sharesToSell <= 0:
+            raise ValueError("Number of shares must be positive")
+
+        if sharesToSell > self.shares[stock_TSLA]:
+            raise ValueError("Attempted to sell more stock than is available")
+
+        self.shares[stock_TSLA] -= sharesToSell
+        self.cash += (1 - Market.transaction_fee) * sharesToSell * curMarket.stocks[stock_TSLA]
+
+    def buy(self, stock_TSLA: str, sharesToBuy: float, curMarket: Market) -> None:
+        if sharesToBuy <= 0:
+            raise ValueError("Number of shares must be positive")
+
+        cost = (1 + Market.transaction_fee) * sharesToBuy * curMarket.stocks[stock_TSLA]
+        if cost > self.cash:
+            raise ValueError("Attempted to spend more cash than available")
+
+        self.shares[stock_TSLA] += sharesToBuy
+        self.cash -= cost
+
+class Context:
+    def __init__(self) -> None:
+        pass
+
+    # PUT WHATEVER YOU WANT HERE
+
+def update_portfolio(curMarket: Market, curPortfolio: Portfolio, context: Context) -> None:
+
+    # YOUR TRADING STRATEGY GOES HERE
+    pass
+
+
+###SIMULATION###
+market = Market()
+portfolio = Portfolio()
+context = Context()
+
+for i in range(365):
+    update_portfolio(market, portfolio, context)
+    market.updateMarket()
+
+print(portfolio.evaluate(market))
+
+def main():
+
+
+
+    # Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+
+    # See PyCharm help at https://www.jetbrains.com/help/pycharm/
